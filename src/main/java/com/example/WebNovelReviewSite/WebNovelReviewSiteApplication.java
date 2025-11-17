5467441c8ce717f65f8ebe5ed76321fa11112967
김준호 <2171168@hansung.ac.kr>
2025-11-17 15:14:14 +0900

    feat: settings

---
+++ src/main/java/com/example/WebNovelReviewSite/WebNovelReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebNovelReviewSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNovelReviewSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebNovelReviewSiteApplication.class, args);
+	}
+
+}
