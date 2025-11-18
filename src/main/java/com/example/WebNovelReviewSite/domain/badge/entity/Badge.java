0a5f6d386849c9640621ef7395f689fffc490c8e
hyunseop <gustjq2580@gmail.com>
2025-11-18 20:20:11 +0900

    엔티티 수정 ongoing

--- src/main/java/com/example/WebNovelReviewSite/domain/badge/entity/Badge.java
+++ src/main/java/com/example/WebNovelReviewSite/domain/badge/entity/Badge.java
@@ -2,35 +2,39 @@ package com.example.WebNovelReviewSite.domain.badge.entity;
 
 
 import com.example.WebNovelReviewSite.domain.badge.enums.BadgeType;
-import com.example.WebNovelReviewSite.domain.user.entity.User;
+import com.example.WebNovelReviewSite.domain.user.entity.UserBadge;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Table(name = "badge")
+@Builder
 public class Badge {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "badge_id")
     private Long badgeId;
 
-    @Column(name = "badge_name",length = 20)
+    @Column(name = "badge_name", length = 20)
     private String badgeName;
-    
-    @Column(name = "badge_image",length = 255)
+
+    @Column(name = "badge_image", length = 255)
     private String badgeImage;
 
     @Column(name = "badge_type")
     @Enumerated(EnumType.STRING)
     private BadgeType badgeType;
 
-    @Column(name = "badge_mission",length = 30)
+    @Column(name = "badge_mission", length = 30)
     private String badgeMission;
 
     @Column(name = "condition_value")
@@ -42,7 +46,7 @@ public class Badge {
     @Column(name = "end_date")
     private LocalDateTime endDate;
 
-    //뱃지 - 유저 관계
-    @ManyToMany(mappedBy = "badges")
-    private Set<User> users = new HashSet<>();
+    // badge - user_badge
+    @OneToMany(mappedBy = "badge", fetch = FetchType.LAZY)
+    private List<UserBadge> userBadges = new ArrayList<>();
 }
--- src/main/java/com/example/WebNovelReviewSite/domain/user/entity/User.java
+++ src/main/java/com/example/WebNovelReviewSite/domain/user/entity/User.java
@@ -39,26 +39,16 @@ public class User {
     @Column(name ="role")
     private Role role;
 
-    //유저 - 뱃지
-    @ManyToMany
-    @JoinTable(
-            name ="user_badge",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "badge_id")
-    )
-    private Set<Badge> badges = new HashSet<>();
+    //user - user_badge
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<UserBadge> userBadges = new  ArrayList<>();
 
-    //팔로우 관계
-    @ManyToMany
-    @JoinTable(
-            name = "follow",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "target_id")
-    )
-    private Set<User>  followings = new HashSet<>();
+    //user - follow
+    @OneToMany(mappedBy = "follower")
+    private Set<Follow> followings = new HashSet<>();
 
-    @ManyToMany(mappedBy = "followings")
-    private Set<User> followers = new HashSet<>();
+    @OneToMany(mappedBy = "target")
+    private Set<Follow> followers = new HashSet<>();
 
     //유저 - 작가
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
